94752d05a6c6f008a2c0d332a3fe63a606859a28
Minh Huy Loi <126984125+mhuy07092004@users.noreply.github.com>
2025-01-24 23:49:31 +1100

    Update assignment 3
    complete all the graph, still have bugs in user input

--- assignment3.py
+++ assignment3.py
@@ -1,68 +1,67 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# Person Class Includes Theirs Information
-class Person:
-    def __init__(self, age, gender, weight, height):
-        self.age = age
-        self.gender = gender.lower()
-        self. weight = weight
-        self. height = height
-    def validate(self):
-        if not (0 < self.age <120):
-            raise ValueError("Invalid Age Please Enter Again!")
-        if self.gender not in ["male", "female"]:
-            raise ValueError("Invalid Gender Please Enter Again!")
-        if not (20< self.weight <200):
-            raise ValueError("Invalid Weight Please Enter Again!")
-        if not (50< self.height <250):
-            raise ValueError("Invalid Height Please Enter Again!")
-
-#     Tracker Class To Track Their BMI and Daily intakes
-class DietTracker:
-    def __init__(self,vegetables, fruits, grains, meats, dairy, bmi):
-        self.daily_vegetables = vegetables
-        self.daily_fruits = fruits
-        self.daily_grains = grains
-        self.daily_meats = meats
-        self.daily_dairy = dairy
-        self.bmi = bmi
-    def validate(self):
-        if not (0 < self.daily_vegetables <20):
-            raise ValueError("Invalid Input Please Enter Again!")
-        if not (0 < self.daily_fruits <20):
-            raise ValueError("Invalid Input Please Enter Again!")
-        if not (0 < self.daily_grains <20):
-            raise ValueError("Invalid Input Please Enter Again!")
-        if not (0 < self.daily_meats <20):
-            raise ValueError("Invalid Input Please Enter Again!")
-        if not (0 < self.daily_dairy <20):
-            raise ValueError("Invalid Input Please Enter Again!")
-        if not (5 < self.bmi <45):
-            raise ValueError("Invalid Input Please Enter Again!")
+import unittest
 
 
-#   User Input Function
-def user_input():
-    try:
-        user_age = int(input("Enter your age: "))
-        user_gender = input("Enter your gender (male or female only not Gmail :> ): ")
-        user_weight = int(input("Enter your weight(kg): "))
-        user_height = int(input("Enter your height(cm): "))
+from assignment3 import Person, DietTracker
 
-        user_vegetables = int(input("Enter your daily intake of vegetables:  "))
-        user_fruits = int(input("Enter your daily intake of fruits: "))
-        user_grains = int(input("Enter your daily intake of grains: "))
-        user_meats = int(input("Enter your daily intake of meats: "))
-        user_dairy = int(input("Enter your daily intake of dairy "))
-        user_bmi = round(float(input("Enter your BMI: ")))
-        return (Person(user_age, user_gender, user_weight, user_height),
-                DietTracker(user_vegetables,user_fruits,user_grains,user_meats,user_dairy,user_bmi))
 
-    except ValueError as e:(
-        print(f"Error: {e}"))
-    except Exception as e:(
-        print(f"An unexpected error occurred: {e}"))
+class TestPerson(unittest.TestCase):
+    def test_valid_person(self):
+#       Test Male
+        male = Person(15, "male",70, 185)
+        male.validate()
+        self.assertEqual(male.age, 15)
+        self.assertEqual(male.gender, "male")
+        self.assertEqual(male.weight, 70)
+        self.assertEqual(male.height, 185)
 
-# Main Programs
-    user_input();
+#       Test Female
+        female = Person(24, "female", 45, 155)
+        female.validate()
+        self.assertEqual(female.age, 24)
+        self.assertEqual(female.gender, "female")
+        self.assertEqual(female.weight, 45)
+        self.assertEqual(female.height, 155)
+
+
+    def test_invalid_age(self):
+    # Test the Person class for invalid age input
+        invalid_ages = [-1, 0, 120, 150]
+        for age in invalid_ages:
+            with self.assertRaises(ValueError):
+                person = Person(age, "male" ,55,170)
+                person.validate()
+    # Test the person gender invalid input
+    def test_invalid_gender(self):
+        """Test the Person class for invalid gender input"""
+        with self.assertRaises(ValueError):
+            person = Person(25, "Gmail", 65,190)
+            person.validate()
+    def test_invalid_weight(self):
+        """Test the Person class for invalid gender input"""
+        with self.assertRaises(ValueError):
+            person = Person(25, "male", 250,190)
+            person.validate()
+    def test_invalid_height(self):
+        """Test the Person class for invalid gender input"""
+        with self.assertRaises(ValueError):
+            person = Person(25, "male", 160,1)
+            person.validate()
+
+class TestDietTracker(unittest.TestCase):
+    def test_valid_diet_input(self):
+        male_diet = DietTracker(5, 4,4, 3,2,27.46)
+        male_diet.validate()
+        self.assertEqual(male_diet.daily_vegetables, 5)
+        self.assertEqual(male_diet.daily_fruits, 4)
+        self.assertEqual(male_diet.daily_grains, 4)
+        self.assertEqual(male_diet.daily_meats, 3)
+        self.assertEqual(male_diet.daily_dairy, 2)
+        self.assertEqual(male_diet.bmi,27.46)
+
+    def test_invalid_diet(self):
+        """Test the Person class for invalid gender input"""
+        invalid_input = [-1, 0, 45]
+        for vegetables in invalid_input:
+            with self.assertRaises(ValueError):
+               user_input  = DietTracker(vegetables, 10, 2,3,4)
+                DietTracker.validate()
